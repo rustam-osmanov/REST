25d7134d52f71227f012fbf44362a8f7ecb9e64e
Rustam Osmanov <rustam-osmanov@yandex.ru>
2026-05-29 14:04:36 +0300

    REST

--- src/main/java/ru/rustamosmanov/spring/rest/controller/MyRESTController.java
+++ src/main/java/ru/rustamosmanov/spring/rest/controller/MyRESTController.java
@@ -37,4 +37,38 @@ public class MyRESTController {
         }
         return employeeBD;
     }
+
+    @PostMapping("/employees")
+    public EmployeeBD addNewEmployee(@RequestBody  EmployeeBD employeeBD) {
+        if (employeeBD == null || employeeBD.getId() != null) {
+            throw new NoSuchEmployeeException("Неверный формат данных для данного api post /employees " );
+        }
+
+        employeeService.saveEmployee(employeeBD);
+        return employeeBD;
+    }
+
+    @PutMapping("/employees")
+    public EmployeeBD updateEmployee(@RequestBody  EmployeeBD employeeBD) {
+        if (employeeBD == null || employeeBD.getId() == null) {
+            throw new NoSuchEmployeeException("Неверный формат данных для данного api put /employees " );
+        }
+        if (employeeService.getEmployee(employeeBD.getId())  == null) {
+            throw new NoSuchEmployeeException("Некорректный формат данных для данного api put /employees " );
+        }
+        employeeService.saveEmployee(employeeBD);
+        return employeeBD;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public EmployeeBD deleteEmployeeById(@PathVariable("id") int id) {
+        EmployeeBD employeeBD = employeeService.getEmployee(id);
+        if (employeeBD == null) {
+            throw new NoSuchEmployeeException("Указанный ID =" + id
+                    + " не найден в БД!");
+        }
+        employeeService.deleteEmployee(employeeBD);
+        return employeeBD;
+    }
+
 }
